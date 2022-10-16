4580f4f516bc53d299b7a703f83c3d53df5f276d
samof <samofalov.19982805@gmail.com>
2022-10-16 13:08:33 -0400

    created Home page and Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import Home from './pages/Home';
+
+const App = () => {
+  return <Home/>;
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import "./Navbar.scss"
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod incidunt quisquam aperiam iste nisi facere perferendis, expedita vitae! Hic minima totam aliquam laudantium cupiditate nobis tenetur non doloremque at temporibus.</div>
+        <div className="center">center</div>
+        <div className="right">right</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
